9ae654be909f7d134263de9741ffbd8a7f59febb
tugba <tugba.berk@markakod.com>
2020-09-14 01:33:24 +0300

    api key was update

--- Crypto/app/src/main/java/com/bitcoin/ticker/data/rest/ServiceUrl.kt
+++ Crypto/app/src/main/java/com/bitcoin/ticker/data/rest/ServiceUrl.kt
@@ -1,7 +1,7 @@
 package com.bitcoin.ticker.data.rest
 
 object ServiceUrls{
-    const val APIKEY="69c7dbddac4dd080c9c462cb239bfdd77469ea75"
+    const val APIKEY="18006b89891cd0760899e3d9c7d20b51760d5301"
     const val BASE_URL = "https://api.cryptoapis.io/v1/"
     const val ASSETS_URL="assets"
     const val ASSETS_DETAIL_URL="assets/{assetId}"
